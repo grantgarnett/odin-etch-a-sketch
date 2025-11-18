8bc45d48f3f5541817cbc79080f8b9d2628c0431
Grant Garnett <grantgarnett01@outlook.com>
2025-11-18 13:09:12 -0600

    Add functions to generate drawing board divs

---
+++ script.js
@@ -0,0 +1,27 @@
+etchContainer = document.querySelector("#etch-container");
+
+function generateSquare(column) {
+    const square = document.createElement("div");
+    square.classList.toggle("square");
+    column.appendChild(square);
+}
+
+function generateColumn(columnSize) {
+    const column = document.createElement("div");
+    column.classList.toggle("column");
+
+    for(i = 0; i < columnSize; i++) {
+        generateSquare(column);
+    }
+
+    etchContainer.appendChild(column);
+}
+
+function generateDrawingBoard(squaresAlongSide) {
+    if(+squaresAlongSide && 
+        +squaresAlongSide === Math.floor(Math.abs(+squaresAlongSide))) {
+        for(let i = 0; i < squaresAlongSide; i++) { 
+            generateColumn(squaresAlongSide);
+        }
+    }
+}
